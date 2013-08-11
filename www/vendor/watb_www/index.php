32e0c24c27ca8cae5ddd546f54081c433f651dc9
Andrew Larcombe <andrew@andrewl.net>
2013-08-11 13:38:45 +0100

    Added history and sparklines

--- www/vendor/watb_www/index.php
+++ www/vendor/watb_www/index.php
@@ -8,17 +8,30 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
     ));
 
+$app->get('/station_history/{station_id}', function ($station_id) use ($app) {
+    return $app->json(get_station_history($station_id));
+});
+
 $app->get('/stations', function () use ($app) {
     return $app->json(get_latest_bikes());
 });
 
-
-$app->get('/{type}', function($type) use ($app) {
+$app->get('/{type}/{bbox}', function($type, $bbox) use ($app) {
+  
   if ($type != 'bikes' && $type != 'docks') {
     $type = 'bikes';
   }
-  $vars = array('criteria' => $type);
+
+  $bbox_coords = explode(',', $bbox);
+  
+  if(count($bbox_coords)!=4) {
+    $bbox_coords = explode(',', '51.52,0.01,51.48,-0.19');
+  }
+
+  $vars = array('criteria' => $type, 'bbox' => $bbox_coords);
+
   return $app['twig']->render('index.twig', $vars);
-})->value('type', 'bikes');
+
+})->value('type', 'bikes')->value('bbox','51.52,0.01,51.48,-0.19');
 
 $app->run();
--- www/vendor/watb_www/solr_query.php
+++ www/vendor/watb_www/solr_query.php
@@ -1,5 +1,38 @@
 <?php
+//@TODO: refactor all this into a nice class
 require_once __DIR__.'/../autoload.php';
+
+function get_station_history($station_id) {
+
+  require(__DIR__.'/../../../solr_connection.php');
+
+  // create a client instance
+  $client = new Solarium\Client($config);
+
+  // get a select query instance
+  $query = $client->createQuery($client::QUERY_SELECT);
+
+  $query->setQuery('station_id: ' . $station_id);
+  $query->createFilterQuery('update_time')->setQuery("update_time:[NOW-1DAY TO NOW]");
+  $query->setStart(1);
+  $query->setRows(9999);
+
+  // this executes the query and returns the result
+  $resultset = $client->execute($query);
+
+  $history = array();
+
+  foreach($resultset as $document) {
+    $station_history = new stdClass;
+    $station_history->time = strtotime($document->update_time);
+    $station_history->bikes = $document->bikes;
+    $station_history->docks = $document->docks;
+    $history[] = $station_history;
+  }
+
+  return $history;
+
+}
 
 function get_latest_bikes() {
   require(__DIR__.'/../../../solr_connection.php');
